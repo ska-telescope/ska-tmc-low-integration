60dca6e0bfcf0ab356178f9359329b3a20aac1e1
Muskan Sharma <muskans.ska@gmail.com>
2024-01-03 14:56:44 +0530

    HM-384: Modify configure test case.

--- tests/features/tmc_sdp/configure_to_sdp_subarray.feature
+++ tests/features/tmc_sdp/configure_to_sdp_subarray.feature
@@ -1,11 +1,10 @@
-@XTP-29336
+@XTP-29227 @XTP-29336 @tmc_sdp
 Scenario: Configure a SDP subarray for a scan using TMC
        Given the Telescope is in ON state
-       And TMC subarray in obsState <obsstate>
+       And TMC subarray in obsState IDLE
        When I configure with <scan_type> to the subarray <subarray_id>
        Then the SDP subarray <subarray_id> obsState is READY
        And the TMC subarray <subarray_id> obsState is transitioned to READY
        Examples:
-       | subarray_id    |    scan_type       | obsstate |
-       | 1              |    "science_A"     | IDLE     |
-       | 1              |    "science_A"     | READY    |
+       | subarray_id    |    scan_type    |
+       | 1              |    target:a     |
--- tests/tmc_sdp/test_configure_to_sdp_subarray.py
+++ tests/tmc_sdp/test_configure_to_sdp_subarray.py
@@ -10,13 +10,14 @@ from ska_control_model import ObsState
 from tango import DevState
 
 from tests.resources.test_support.common_utils.tmc_helpers import (
+    prepare_json_args_for_centralnode_commands,
     prepare_json_args_for_commands,
 )
 
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.real_sdp
+@pytest.mark.tmc_sdp
 @pytest.mark.configure
 @scenario(
     "../features/tmc_sdp/configure_to_sdp_subarray.feature",
@@ -42,16 +43,30 @@ def telescope_is_in_on_state(central_node_low, event_recorder):
     )
 
 
-@given(parsers.parse("the TMC subarray {subarray_id} obsState is {obsstate}"))
-def tmc_subarray_is_in_obsstate(
-    subarray_id, obsstate, central_node_low, event_recorder
+
+@given(parsers.parse("TMC subarray in obsState IDLE"))
+def check_subarray_obs_state(
+    central_node_low, event_recorder, command_input_factory
 ):
-    """Verify the specified TMC subarray is in the given observation state."""
+    """Method to check subarray is in IDLE obstate"""
     event_recorder.subscribe_event(central_node_low.subarray_node, "obsState")
+    event_recorder.subscribe_event(
+        central_node_low.subarray_devices.get("sdp_subarray"), "obsState"
+    )
+    assign_input_json = prepare_json_args_for_centralnode_commands(
+        "assign_resources_low", command_input_factory
+    )
+    central_node_low.store_resources(assign_input_json)
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_devices.get("sdp_subarray"),
+        "obsState",
+        ObsState.IDLE,
+    )
     assert event_recorder.has_change_event_occurred(
         central_node_low.subarray_node,
         "obsState",
-        ObsState[obsstate],
+        ObsState.IDLE,
     )
 
 
@@ -59,7 +74,11 @@ def tmc_subarray_is_in_obsstate(
     parsers.parse("I configure with {scan_type} to the subarray {subarray_id}")
 )
 def configure_sdp_subarray(
-    scan_type, subarray_node_low, command_input_factory
+   central_node_low, 
+   scan_type, 
+   subarray_node_low, 
+   command_input_factory, 
+   subarray_id,
 ):
     """Method to configure SDP subarray."""
     configure_input_json = prepare_json_args_for_commands(
@@ -67,36 +86,61 @@ def configure_sdp_subarray(
     )
     configure_input_json = json.loads(configure_input_json)
     configure_input_json["sdp"]["scan_type"] = scan_type
-    configure_input_json = json.dumps(configure_input_json)
+    central_node_low.set_subarray_id(subarray_id)
 
-    subarray_node_low.store_configuration_data(configure_input_json)
+    subarray_node_low.execute_transition("Configure", argin=json.dumps(configure_input_json))
 
-
-@then(parsers.parse("the SDP subarray {subarray_id} obsState is {obsstate}"))
-def check_sdp_is_in_obsstate(
-    subarray_id, obsstate, central_node_low, event_recorder
+@then(parsers.parse("the SDP subarray {subarray_id} obsState is READY"))
+def check_sdp_subarray_in_ready(
+    central_node_mid, subarray_node, event_recorder, subarray_id
 ):
-    """Method to check SDP subarray is in the expected observation state."""
+    """A method to check SDP subarray obsstate"""
     event_recorder.subscribe_event(
-        central_node_low.subarray_devices.get("sdp_subarray"), "obsState"
-    )
-    assert event_recorder.has_change_event_occurred(
-        central_node_low.subarray_devices.get("sdp_subarray"),
-        "obsState",
-        ObsState[obsstate],
+        subarray_node.subarray_devices["sdp_subarray"], "obsState"
     )
 
+    central_node_mid.set_subarray_id(subarray_id)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_devices["sdp_subarray"],
+        "obsState",
+        ObsState.READY,
+    )
+
+
+@then(
+    parsers.parse(
+        "the SDP subarray {subarray_id} obsState is READY"
+    )
+)
+def check_sdp_subarray_in_ready(
+    central_node_low, subarray_node, event_recorder, subarray_id
+):
+    """A method to check SDP subarray obsstate"""
+    event_recorder.subscribe_event(
+        subarray_node.subarray_devices["sdp_subarray"], "obsState"
+    )
+
+    central_node_low.set_subarray_id(subarray_id)
+    assert event_recorder.has_change_event_occurred(
+        subarray_node.subarray_devices["sdp_subarray"],
+        "obsState",
+        ObsState.READY,
+    )
 
 @then(
     parsers.parse(
         "the TMC subarray {subarray_id} obsState is transitioned to READY"
     )
 )
-def check_tmc_is_in_ready_obsstate(central_node_low, event_recorder):
-    """Method to check TMC subarray is in the READY observation state."""
-    event_recorder.subscribe_event(central_node_low.subarray_node, "obsState")
+def check_tmc_subarray_obs_state(
+    central_node_low, subarray_node, event_recorder, subarray_id
+):
+    """A method to check TMC subarray obsstate"""
+    event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
+
+    central_node_low.set_subarray_id(subarray_id)
     assert event_recorder.has_change_event_occurred(
-        central_node_low.subarray_node,
+        subarray_node.subarray_node,
         "obsState",
         ObsState.READY,
     )
