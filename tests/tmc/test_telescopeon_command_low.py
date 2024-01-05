b38fca50a6150a62d0190050f525bd426de7199b
akshaysonawane <akshayss.ska@gmail.com>
2024-01-05 16:19:45 +0530

    HM-385: test pipeline

--- tests/tmc/test_telescopeon_command_low.py
+++ tests/tmc/test_telescopeon_command_low.py
@@ -19,6 +19,7 @@ from tests.resources.test_support.constant_low import (
 )
 
 
+@pytest.mark.test_on
 @pytest.mark.SKA_low
 @pytest.mark.skip(reason="Unskip after repository setup")
 def test_telescope_on():
