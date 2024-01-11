ed3309e20cc7930519549a8f76870da8e144a603
Jatin Sharma <jatins.ska@gmail.com>
2024-01-11 17:03:50 +0530

    HM-386:Adding scenarios and test cases for abort resourcing

---
+++ tests/features/tmc_sdp/abort_resourcing.feature
@@ -0,0 +1,7 @@
+@XTP-29582 @tmc_sdp
+Scenario: Abort configuring SDP using TMC
+        Given TMC and SDP subarray busy assigning resources
+        When I command it to Abort
+        Then the SDP subarray should go into an aborted obsstate
+        And the TMC subarray obsState transitions to ABORTED
+        
---
+++ tests/tmc_sdp/test_assign_abort.py
@@ -0,0 +1,105 @@
+"""Test TMC-SDP Abort functionality in RESOURCING obsState"""
+import pytest
+from pytest_bdd import given, parsers, scenario, then, when
+from ska_control_model import ObsState
+from tango import DevState
+import json
+from tests.resources.test_harness.helpers import (
+    update_eb_pb_ids,
+    prepare_json_args_for_centralnode_commands,
+)
+
+@pytest.mark.test1
+@pytest.mark.tmc_sdp
+@scenario(
+    "../features/tmc_sdp/abort_resourcing.feature",
+    "Abort configuring SDP using TMC",
+)
+def test_tmc_sdp_abort_stuck_in_resourcing(central_node_low):
+    """
+    Test case to verify TMC-SDP Abort functionality in RESOURCING obsState
+    """
+    assert central_node_low.central_node.ping() > 0
+    assert central_node_low.subarray_devices["sdp_subarray"].ping() > 0
+
+
+@given(
+    parsers.parse(
+        "TMC and SDP subarray busy assigning resources"
+    )
+)
+def telescope_is_in_resourcing_obsstate(
+    central_node_low, event_recorder, command_input_factory
+):
+    """A method to check if telescope in is resourcing obsSstate."""
+    central_node_low.move_to_on()
+    event_recorder.subscribe_event(
+        central_node_low.central_node, "telescopeState"
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.central_node,
+        "telescopeState",
+        DevState.ON,
+    )
+    assign_input_json = prepare_json_args_for_centralnode_commands(
+        "assign_resources_low", command_input_factory
+    )
+    input_json=update_eb_pb_ids(assign_input_json)
+    central_node_low.perform_action("AssignResources", input_json)
+
+    event_recorder.subscribe_event(
+        central_node_low.subarray_devices.get("sdp_subarray"), "obsState"
+    )
+    event_recorder.subscribe_event(central_node_low.subarray_node, "obsState")
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_node,
+        "obsState",
+        ObsState.RESOURCING,
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_devices.get("sdp_subarray"),
+        "obsState",
+        ObsState.RESOURCING,
+    )
+
+
+@when("I command it to Abort")
+def abort_is_invoked(central_node_low):
+    """
+    This method invokes abort command on tmc subarray
+    """
+    central_node_low.subarray_abort()
+
+
+@then(
+    parsers.parse(
+        "the SDP subarray should go into an aborted obsstate"
+    )
+)
+def sdp_subarray_is_in_aborted_obsstate(
+    central_node_low, event_recorder
+):
+    """
+    Method to check SDP subarray is in ABORTED obsstate
+    """
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_devices.get("sdp_subarray"),
+        "obsState",
+        ObsState.ABORTED,
+    )
+
+
+@then(
+    "the TMC subarray obsState transitions to ABORTED"
+)
+def tmc_subarray_is_in_aborted_obsstate(
+    central_node_low, event_recorder
+):
+    """
+    Method to check if TMC subarray is in ABORTED obsstate
+    """
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_node,
+        "obsState",
+        ObsState.ABORTED,
+    )
