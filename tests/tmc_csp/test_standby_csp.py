91a47c604f8669bc6d078a2d01f5ce9cb8370140
akshaysonawane <akshayss.ska@gmail.com>
2024-01-15 14:25:29 +0530

    HM-394: add standby test case

--- tests/tmc_csp/test_standby_csp.py
+++ tests/tmc_csp/test_standby_csp.py
@@ -3,7 +3,10 @@ import pytest
 from pytest_bdd import given, scenario, then, when
 from tango import DevState
 
+from tests.resources.test_harness.helpers import get_master_device_simulators
 
+
+@pytest.mark.aki
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/tmc_csp_standby.feature",
@@ -21,10 +24,23 @@ def test_tmc_sdp_standby_telescope():
 
 
 @given("a Telescope consisting of TMC,CSP,simulated SDP and simulated MCCS")
-def check_tmc_and_sdp_is_on(central_node_low, event_recorder):
+def check_tmc_and_sdp_is_on(
+    central_node_low, event_recorder, simulator_factory
+):
     """
     Given a TMC and CSP in ON state
     """
+    (
+        _,
+        sdp_master_sim,
+    ) = get_master_device_simulators(simulator_factory)
+
+    assert central_node_low.central_node.ping() > 0
+    assert central_node_low.sdp_master.ping() > 0
+    assert central_node_low.subarray_devices["sdp_subarray"].ping() > 0
+    assert sdp_master_sim.ping() > 0
+    central_node_low.csp_master.adminMode = 0
+    central_node_low.csp_subarray1.adminMode = 0
     event_recorder.subscribe_event(
         central_node_low.central_node, "telescopeState"
     )
@@ -64,7 +80,7 @@ def move_sdp_to_standby(central_node_low):
     central_node_low.set_standby()
 
 
-@then("telescope state is standby")
+@then("telescope state is STANDBY")
 def check_telescope_state_standby(central_node_low, event_recorder):
     """A method to check CentralNode.telescopeState"""
     assert event_recorder.has_change_event_occurred(
