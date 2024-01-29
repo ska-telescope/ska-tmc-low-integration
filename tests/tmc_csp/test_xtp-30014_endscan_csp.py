0ad682d892aa8fe62facfdc3031de3055ad5469f
Muskan Sharma <muskans.ska@gmail.com>
2024-01-29 20:06:54 +0530

    HM-403: Modified Test case of EndScan Command.

--- tests/tmc_csp/test_xtp-30014_endscan_csp.py
+++ tests/tmc_csp/test_xtp-30014_endscan_csp.py
@@ -19,7 +19,7 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 
 @pytest.mark.tmc_csp
 @scenario(
-    "../features/tmc_csp/xtp-30014_endscan_csp.feature",
+    "../features/tmc_csp/xtp-30014_end_scan_csp.feature",
     "TMC executes a EndScan command on CSP subarray.",
 )
 def test_tmc_csp_endscan_functionality():
