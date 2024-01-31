23b472292385aaf473438b9bceb6bd44b472c8d2
Jatin Sharma <jatins.ska@gmail.com>
2024-01-31 14:34:00 +0530

    HM-402:resolving review comments

--- tests/tmc_sdp/test_abort_configuring.py
+++ tests/tmc_sdp/test_abort_configuring.py
@@ -54,7 +54,8 @@ def subarray_is_in_configuring_obsstate(
         "assign_resources_low", command_input_factory
     )
     assign_str = json.loads(assign_input_json)
-    # Adding this to get an event of ObsState CONFIGURING from SDP
+    # "time-to-ready" is added in AssignResources input JSON to
+    # receive an event of ObsState CONFIGURING from SDP Subarray
     assign_str["sdp"]["processing_blocks"][0]["parameters"][
         "time-to-ready"
     ] = 2
