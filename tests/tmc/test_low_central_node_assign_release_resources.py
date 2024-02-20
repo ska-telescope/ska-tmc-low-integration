4edd0eced884c627d9662fafcc8f7e80d6b1e678
Bhushan Parakh <bhushan.ska@gmail.com>
2024-02-20 22:09:03 +0530

    HM-407: Fixing SDP job failures. Removing sleep

--- tests/tmc/test_low_central_node_assign_release_resources.py
+++ tests/tmc/test_low_central_node_assign_release_resources.py
@@ -8,7 +8,6 @@ verification of attribute updates, exception handling, and proper propagation
 of exceptions.
 """
 import json
-import time
 
 import pytest
 from ska_control_model import ObsState
@@ -413,10 +412,6 @@ class TestLowCentralNodeAssignResources:
             "longRunningCommandResult",
             (unique_id[0], str(ResultCode.OK.value)),
         )
-        # Allowing Subarray Node to finish processing the Assign command
-        # completion. Removing the sleep leads to timer thread for Release
-        # being stopped by Assign cleanup
-        time.sleep(1)
 
         # Setting device to defective
         sdp_subarray_sim.SetDefective(json.dumps(FAILED_RESULT_DEFECT))
