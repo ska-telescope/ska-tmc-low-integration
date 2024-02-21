9a4663f8e76f481eb459bb256da2e426b18fd447
Jatin Sharma <jatins.ska@gmail.com>
2024-02-21 10:11:22 +0530

    HM-390:resolving review commentes

--- tests/tmc/test_configure_invalid_json_low.py
+++ tests/tmc/test_configure_invalid_json_low.py
@@ -54,7 +54,7 @@ def tmc_check_status(event_recorder, central_node_low, command_input_factory):
     assign_input_json = prepare_json_args_for_centralnode_commands(
         "assign_resources_low", command_input_factory
     )
-    central_node_low.central_node.AssignResources(assign_input_json)
+    central_node_low.store_resources(assign_input_json)
     assert event_recorder.has_change_event_occurred(
         central_node_low.subarray_node, "obsState", ObsState.RESOURCING
     )
