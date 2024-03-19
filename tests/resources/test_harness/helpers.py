a2da2192bba992ea1eda476d46d8e32055757666
akshaysonawane <akshayss.ska@gmail.com>
2024-03-19 17:07:10 +0530

    HM-435: update the adminmode settings

--- tests/resources/test_harness/helpers.py
+++ tests/resources/test_harness/helpers.py
@@ -516,10 +516,31 @@ def set_admin_mode_values_mccs():
     if MCCS_SIMULATION_ENABLED.lower() == "false":
         controller = tango.DeviceProxy(mccs_controller)
         if controller.adminMode != 0:
+            #     db = tango.Database()
+            #     device_strings = db.get_device_exported("low-mccs/*")
+            #     devices = []
+            #     for device_str in device_strings:
+            #         device = tango.DeviceProxy(device_str)
+            #         device.adminMode = 0
+            #         devices.append(device)
+
             db = tango.Database()
-            device_strings = db.get_device_exported("low-mccs/*")
+
+            pasd_bus_trls = db.get_device_exported("low-mccs/pasdbus/*")
+            for pasd_bus_trl in pasd_bus_trls:
+                pasdbus = tango.DeviceProxy(pasd_bus_trl)
+                if pasdbus.adminmode != 0:
+                    pasdbus.adminmode = 0
+                    time.sleep(0.1)
+
+            device_trls = db.get_device_exported("low-mccs/*")
             devices = []
-            for device_str in device_strings:
-                device = tango.DeviceProxy(device_str)
-                device.adminMode = 0
-                devices.append(device)
+
+            for device_trl in device_trls:
+                if "daq" in device_trl or "calibrationstore" in device_trl:
+                    continue
+                device = tango.DeviceProxy(device_trl)
+                if device.adminmode != 0:
+                    device.adminmode = 0
+                    devices.append(device)
+                    time.sleep(0.1)
