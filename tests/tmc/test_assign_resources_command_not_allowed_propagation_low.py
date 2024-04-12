fb719a189dbafa3eadea9a37df4cd4a3b757eb7d
Muskan Sharma <muskans.ska@gmail.com>
2024-04-12 11:12:00 +0530

    HM-438: added command_not_allowed test case.

--- tests/tmc/test_assign_resources_command_not_allowed_propagation_low.py
+++ tests/tmc/test_assign_resources_command_not_allowed_propagation_low.py
@@ -15,8 +15,6 @@ class TestAssignCommandNotAllowedPropagation:
     """Test the command not allowed error propagation for the assign resources
     command for TMC."""
 
-    # TODO: Add test for MCCS after Error Propagation is updated in Subarray
-    # Node to function for MCCS Devices
     @pytest.mark.SKA_low
     def test_assign_command_not_allowed_propagation_csp_ln_low(
         self,
@@ -77,6 +75,7 @@ class TestAssignCommandNotAllowedPropagation:
         )
 
     @pytest.mark.SKA_low
+    @pytest.mark.test
     def test_assign_command_not_allowed_propagation_sdp_ln_low(
         self,
         central_node_low,
@@ -120,11 +119,71 @@ class TestAssignCommandNotAllowedPropagation:
         # Setting Defects on Devices
         sdp_subarray_sim.SetDefective(COMMAND_NOT_ALLOWED_DEFECT)
         _, unique_id = central_node_low.store_resources(assign_input_json)
-        ERROR_MESSAGE = (
-            "Exception occurred on the following devices:"
-            "\nska_low/tm_leaf_node/sdp_subarray01: ska_tmc_common."
-            "exceptions.CommandNotAllowed: Command is not allowed"
+        ERROR_MESSAGE = """Exception occurred on the following
+            devices: ska_low/tm_leaf_node/sdp_subarray01:
+            ska_tmc_common.exceptions.CommandNotAllowed:
+            Command is not allowed\n\n"""
+        assertion_data = event_recorder.has_change_event_occurred(
+            central_node_low.central_node,
+            "longRunningCommandResult",
+            (unique_id[0], Anything),
         )
+        assert ERROR_MESSAGE in assertion_data["attribute_value"][1]
+        event_recorder.has_change_event_occurred(
+            central_node_low.central_node,
+            "longRunningCommandResult",
+            (unique_id[0], str(ResultCode.FAILED.value)),
+        )
+
+    @pytest.mark.SKA_low
+    def test_assign_command_not_allowed_propagation_mccs_ln_low(
+        self,
+        central_node_low,
+        event_recorder,
+        command_input_factory,
+        simulator_factory,
+    ):
+        """Verify command not allowed exception propagation from MccsLeafNodes
+        ."""
+        sdp_subarray_sim = simulator_factory.get_or_create_simulator_device(
+            SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
+        )
+        mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
+            SimulatorDeviceType.LOW_SDP_DEVICE
+        )
+
+        # Event Subscriptions
+        event_recorder.subscribe_event(
+            central_node_low.central_node, "telescopeState"
+        )
+        event_recorder.subscribe_event(
+            central_node_low.subarray_node, "obsState"
+        )
+        event_recorder.subscribe_event(
+            central_node_low.central_node, "longRunningCommandResult"
+        )
+        event_recorder.subscribe_event(mccs_subarray_sim, "obsState")
+        event_recorder.subscribe_event(sdp_subarray_sim, "obsState")
+        # Preparing input arguments
+        assign_input_json = prepare_json_args_for_centralnode_commands(
+            "assign_resources_low", command_input_factory
+        )
+
+        central_node_low.move_to_on()
+        assert event_recorder.has_change_event_occurred(
+            central_node_low.central_node,
+            "telescopeState",
+            DevState.ON,
+        )
+
+        # Setting Defects on Devices
+        mccs_subarray_sim.SetDefective(COMMAND_NOT_ALLOWED_DEFECT)
+        _, unique_id = central_node_low.store_resources(assign_input_json)
+        # Constructing the error message
+        ERROR_MESSAGE = """Exception occurred on the following
+            devices: ska_low/tm_leaf_node/mccs_subarray01:
+            ska_tmc_common.exceptions.CommandNotAllowed:
+            Command is not allowed\n\n"""
         assertion_data = event_recorder.has_change_event_occurred(
             central_node_low.central_node,
             "longRunningCommandResult",
