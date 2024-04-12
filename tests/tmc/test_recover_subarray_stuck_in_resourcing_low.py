a7230bea8d2bc6b24873c5c8d753868f3abc8d39
Jatin Sharma <jatins.ska@gmail.com>
2024-04-12 15:53:42 +0530

    SKB-230:Adding test case for resolving skb-230

--- tests/tmc/test_recover_subarray_stuck_in_resourcing_low.py
+++ tests/tmc/test_recover_subarray_stuck_in_resourcing_low.py
@@ -120,7 +120,7 @@ def test_recover_subarray_stuck_in_resourcing_low(
 
 @pytest.mark.SKA_low
 @pytest.mark.parametrize("defective_device", ["csp_subarray", "sdp_subarray"])
-def test_abort_with_sdp_in_empty(
+def test_abort_with_sdp_csp_in_empty(
     event_recorder,
     central_node_low,
     command_input_factory,
@@ -220,6 +220,93 @@ def test_abort_with_sdp_in_empty(
         ObsState.IDLE,
     )
     defective_device_proxy.SetDefective(json.dumps({"enabled": False}))
+
+    central_node_low.subarray_node.Abort()
+
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_node,
+        "obsState",
+        ObsState.ABORTED,
+    )
+
+
+@pytest.mark.test
+@pytest.mark.SKA_low
+def test_abort_with_mccs_in_empty(
+    event_recorder,
+    central_node_low,
+    command_input_factory,
+):
+    """recover subarray when sdp is in empty with abort."""
+    event_recorder.subscribe_event(
+        central_node_low.central_node, "telescopeState"
+    )
+    assign_input_json = prepare_json_args_for_centralnode_commands(
+        "assign_resources_low", command_input_factory
+    )
+    event_recorder.subscribe_event(
+        central_node_low.central_node, "longRunningCommandResult"
+    )
+    event_recorder.subscribe_event(central_node_low.subarray_node, "obsState")
+    event_recorder.subscribe_event(
+        central_node_low.subarray_devices["csp_subarray"], "obsState"
+    )
+    event_recorder.subscribe_event(
+        central_node_low.subarray_devices["sdp_subarray"], "obsState"
+    )
+    event_recorder.subscribe_event(
+        central_node_low.subarray_devices["mccs_subarray"], "obsState"
+    )
+    central_node_low.move_to_on()
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.central_node,
+        "telescopeState",
+        DevState.ON,
+    )
+
+    # Set SDP into FAILED RESULT to change from RESOURCING to EMPTY
+    failed_result_defect = FAILED_RESULT_DEFECT
+    failed_result_defect["target_obsstates"] = [
+        ObsState.RESOURCING,
+        ObsState.EMPTY,
+    ]
+    mccs_device_proxy = central_node_low.subarray_devices.get("mccs_subarray")
+    mccs_device_proxy.SetDefective(json.dumps(failed_result_defect))
+
+    assert wait_and_validate_device_attribute_value(
+        mccs_device_proxy,
+        "defective",
+        json.dumps(failed_result_defect),
+        is_json=True,
+    )
+    _, unique_id = central_node_low.store_resources(assign_input_json)
+
+    assertion_data = event_recorder.has_change_event_occurred(
+        central_node_low.central_node,
+        "longRunningCommandResult",
+        (unique_id[0], Anything),
+    )
+    assert (
+        "Exception occurred on the following devices:"
+        + " ska_low/tm_subarray_node/1"
+        in assertion_data["attribute_value"][1]
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_low.subarray_node,
+        "obsState",
+        ObsState.RESOURCING,
+    )
+    assert event_recorder.has_change_event_occurred(
+        mccs_device_proxy,
+        "obsState",
+        ObsState.RESOURCING,
+    )
+    assert event_recorder.has_change_event_occurred(
+        mccs_device_proxy,
+        "obsState",
+        ObsState.EMPTY,
+    )
+    mccs_device_proxy.SetDefective(json.dumps({"enabled": False}))
 
     central_node_low.subarray_node.Abort()
 
