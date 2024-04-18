5b147c6067a6233bce2119b1be7761c799e90a61
Akshay Sonawane <akshayss.ska@gmail.com>
2024-04-18 07:59:54 +0530

    HM-455: skip the test case

--- tests/tmc_sdp/test_xtp_34886_sdp_json_error_duplicate_id.py
+++ tests/tmc_sdp/test_xtp_34886_sdp_json_error_duplicate_id.py
@@ -17,6 +17,7 @@ from tests.resources.test_harness.helpers import (
 )
 
 
+@pytest.mark.skip
 @pytest.mark.tmc_sdp
 @scenario(
     "../features/tmc_sdp/xtp_34886_sdp_json_error_duplicate_id.feature",
