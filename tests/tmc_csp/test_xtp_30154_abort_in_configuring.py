bdeb030b5c7bc68385831b5f622bb8efa64c185b
devesh.ska <devesh.ska@gmail.com>
2024-07-11 14:59:29 +0530

    HM-486: Update test cases

--- tests/tmc_csp/test_xtp_30154_abort_in_configuring.py
+++ tests/tmc_csp/test_xtp_30154_abort_in_configuring.py
@@ -14,10 +14,6 @@ from tests.resources.test_support.common_utils.tmc_helpers import (
 )
 
 
-@pytest.mark.skip(
-    reason="Issue on CSP side, will get fixed with the new CSP chart"
-)
-# Issue: CSP is not using SKA Tel Model >= v1.17.0
 @pytest.mark.tmc_csp
 @scenario(
     "../features/tmc_csp/xtp-30154_abort_in_configuring.feature",
