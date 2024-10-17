563bb9dd2702beaf700769321f2686e538f1fae6
Muskan Sharma <muskans.ska@gmail.com>
2024-10-17 17:08:16 +0530

    HM-558:Update jsons.

--- tests/tmc/test_low_central_node_assign_release_resources.py
+++ tests/tmc/test_low_central_node_assign_release_resources.py
@@ -24,7 +24,6 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 from tests.resources.test_support.common_utils.tmc_helpers import (
     prepare_json_args_for_centralnode_commands,
-    prepare_json_args_for_commands,
 )
 from tests.resources.test_support.constant_low import (
     INTERMEDIATE_STATE_DEFECT,
@@ -38,7 +37,7 @@ class TestLowCentralNodeAssignResources:
     simulated master devices (CSP, SDP, MCCS) and the overall
       telescope state."""
 
-    @pytest.mark.SKA_low
+    @pytest.mark.SKA_low1
     def test_low_centralnode_assign_resources(
         self,
         central_node_low: CentralNodeWrapperLow,
@@ -68,14 +67,31 @@ class TestLowCentralNodeAssignResources:
             "release_resources_low", command_input_factory
         )
 
-        assigned_resources_json = prepare_json_args_for_commands(
-            "AssignedResources_low", command_input_factory
+        assigned_resources_json = {
+            "subarray_beam_ids": ["some_id"],
+            "station_ids": ["station1", "station2"],
+            "apertures": ["AP001.01", "AP001.02"],
+            "channels": [32],
+        }
+
+        assigned_resources_json_empty = {
+            "subarray_beam_ids": [],
+            "station_ids": [],
+            "apertures": [],
+            "channels": [0],
+        }
+
+        # Update transaction and subarray ID
+        assign_input_json["transaction_id"] = "txn-....-00002"
+        assign_input_json["subarray_id"] = 2
+
+        # Convert to JSON strings
+        assigned_resources_json_str = json.dumps(assigned_resources_json)
+        assigned_resources_json_empty_str = json.dumps(
+            assigned_resources_json_empty
         )
 
-        assigned_resources_json_empty = prepare_json_args_for_commands(
-            "AssignedResources_low_empty", command_input_factory
-        )
-
+        # Create simulator devices
         csp_subarray_sim = simulator_factory.get_or_create_simulator_device(
             SimulatorDeviceType.LOW_CSP_DEVICE
         )
@@ -85,20 +101,22 @@ class TestLowCentralNodeAssignResources:
         mccs_controller_sim = simulator_factory.get_or_create_simulator_device(
             SimulatorDeviceType.MCCS_MASTER_DEVICE
         )
-
         mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
             SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
         )
 
-        event_tracer.subscribe_event(csp_subarray_sim, "State")
-        event_tracer.subscribe_event(sdp_subarray_sim, "State")
-        event_tracer.subscribe_event(mccs_controller_sim, "State")
+        # Subscribe to events
+        for sim in [
+            csp_subarray_sim,
+            sdp_subarray_sim,
+            mccs_controller_sim,
+            mccs_subarray_sim,
+        ]:
+            event_tracer.subscribe_event(sim, "State")
+            event_tracer.subscribe_event(sim, "obsState")
         event_tracer.subscribe_event(
             central_node_low.central_node, "telescopeState"
         )
-        event_tracer.subscribe_event(csp_subarray_sim, "obsState")
-        event_tracer.subscribe_event(sdp_subarray_sim, "obsState")
-        event_tracer.subscribe_event(mccs_subarray_sim, "obsState")
         event_tracer.subscribe_event(
             central_node_low.subarray_node, "obsState"
         )
@@ -108,106 +126,53 @@ class TestLowCentralNodeAssignResources:
         event_tracer.subscribe_event(
             central_node_low.central_node, "longRunningCommandResult"
         )
-        log_events(
-            {
-                central_node_low.central_node: [
-                    "longRunningCommandResult",
-                    "telescopeState",
-                ],
-                central_node_low.subarray_node: [
-                    "obsState",
-                    "assignedResources",
-                ],
-            }
-        )
+
         # Execute ON Command
         central_node_low.move_to_on()
 
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER STANDBY COMMAND: "
-            "SDP Subarray device"
-            f"({csp_subarray_sim.dev_name()}) "
-            "is expected to be in State ON",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            csp_subarray_sim,
-            "State",
-            DevState.ON,
-        )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER STANDBY COMMAND: "
-            "MCCS Controller device"
-            f"({sdp_subarray_sim.dev_name()}) "
-            "is expected to be in State ON",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            mccs_controller_sim,
-            "State",
-            DevState.ON,
-        )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ON COMMAND: "
-            "Central Node device"
-            f"({central_node_low.central_node.dev_name()}) "
-            "is expected to be in TelescopeState ON",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            central_node_low.central_node,
-            "telescopeState",
-            DevState.ON,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(csp_subarray_sim, "State", DevState.ON)
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(mccs_controller_sim, "State", DevState.ON)
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            central_node_low.central_node, "telescopeState", DevState.ON
         )
 
-        # Execute Assign command and check command completed successfully
+        # Execute Assign command
         _, unique_id = central_node_low.perform_action(
             "AssignResources", assign_input_json
         )
+        mccs_subarray_sim.SetDirectassignedResources(
+            assigned_resources_json_str
+        )
 
-        mccs_subarray_sim.SetDirectassignedResources(assigned_resources_json)
-
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
-            "SDP Subarray device"
-            f"({sdp_subarray_sim.dev_name()}) "
-            "is expected to be in IDLE obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            sdp_subarray_sim,
-            "obsState",
-            ObsState.IDLE,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            sdp_subarray_sim, "obsState", ObsState.IDLE
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
-            "CSP Subarray device"
-            f"({csp_subarray_sim.dev_name()}) "
-            "is expected to be in IDLE obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            csp_subarray_sim,
-            "obsState",
-            ObsState.IDLE,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            csp_subarray_sim, "obsState", ObsState.IDLE
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
-            "MCCS Subarray device"
-            f"({mccs_subarray_sim.dev_name()}) "
-            "is expected to be in IDLE obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            mccs_subarray_sim,
-            "obsState",
-            ObsState.IDLE,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            mccs_subarray_sim, "obsState", ObsState.IDLE
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGN RESOURCES: "
-            "Subarray Node device"
-            f"({central_node_low.subarray_node.dev_name()}) "
-            "is expected to be in IDLE obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            central_node_low.subarray_node,
-            "obsState",
-            ObsState.IDLE,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            central_node_low.subarray_node, "obsState", ObsState.IDLE
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGNRESOURCES COMMAND: "
-            "Central Node device"
-            f"({central_node_low.central_node.dev_name()}) "
-            "is expected have longRunningCommand as"
-            '(unique_id,(ResultCode.OK,"Command Completed"))',
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
             central_node_low.central_node,
             "longRunningCommandResult",
             (
@@ -215,70 +180,41 @@ class TestLowCentralNodeAssignResources:
                 json.dumps((int(ResultCode.OK), "Command Completed")),
             ),
         )
-
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER ASSIGN RESOURCES: "
-            "Subarray Node device"
-            f"({central_node_low.subarray_node.dev_name()}) "
-            "is expected to have assignedResources input json",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
             central_node_low.subarray_node,
             "assignedResources",
-            (str(assigned_resources_json),),
+            (assigned_resources_json_str,),
         )
 
-        # Execute release command and verify command completed successfully
-
+        # Execute release command
         _, unique_id = central_node_low.perform_action(
             "ReleaseResources", release_resource_json
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "SDP Subarray device"
-            f"({sdp_subarray_sim.dev_name()}) "
-            "is expected to be in EMPTY obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            sdp_subarray_sim,
-            "obsState",
-            ObsState.EMPTY,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            sdp_subarray_sim, "obsState", ObsState.EMPTY
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "CSP Subarray device"
-            f"({csp_subarray_sim.dev_name()}) "
-            "is expected to be in EMPTY obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            csp_subarray_sim,
-            "obsState",
-            ObsState.EMPTY,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            csp_subarray_sim, "obsState", ObsState.EMPTY
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "MCCS Subarray device"
-            f"({mccs_subarray_sim.dev_name()}) "
-            "is expected to be in EMPTY obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            mccs_subarray_sim,
-            "obsState",
-            ObsState.EMPTY,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            mccs_subarray_sim, "obsState", ObsState.EMPTY
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "Subarray Node device"
-            f"({central_node_low.subarray_node.dev_name()}) "
-            "is expected to be in EMPTY obstate",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
-            central_node_low.subarray_node,
-            "obsState",
-            ObsState.EMPTY,
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
+            central_node_low.subarray_node, "obsState", ObsState.EMPTY
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "Central Node device"
-            f"({central_node_low.central_node.dev_name()}) "
-            "is expected have longRunningCommand as"
-            '(unique_id,(ResultCode.OK,"Command Completed"))',
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
             central_node_low.central_node,
             "longRunningCommandResult",
             (
@@ -288,19 +224,15 @@ class TestLowCentralNodeAssignResources:
         )
 
         # Setting Assigned Resources empty
-
         mccs_subarray_sim.SetDirectassignedResources(
-            assigned_resources_json_empty
+            assigned_resources_json_empty_str
         )
-        assert_that(event_tracer).described_as(
-            "FAILED ASSUMPTION AFTER RELEASE_RESOURCES COMMAND: "
-            "Subarray Node device"
-            f"({central_node_low.subarray_node.dev_name()}) "
-            "is expected assignedResources to be empty",
-        ).within_timeout(TIMEOUT).has_change_event_occurred(
+        assert_that(event_tracer).within_timeout(
+            TIMEOUT
+        ).has_change_event_occurred(
             central_node_low.subarray_node,
             "assignedResources",
-            (str(assigned_resources_json_empty),),
+            (assigned_resources_json_empty_str,),
         )
 
     @pytest.mark.SKA_low
